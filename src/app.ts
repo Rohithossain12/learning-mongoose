44dca73ad3f204ef0d636e3a7b1bc1b848d7fb4d
Rohithossain12 <https://github.com/Rohithossain12>
2025-06-18 15:13:58 +0600

    create schema and modle

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,31 @@
 import express, { Application, Request, Response } from "express";
+import { model, Schema } from "mongoose";
 
 const app: Application = express();
+
+// create schema
+const noteSchema = new Schema({
+    title: String,
+    content: String
+})
+
+// create model
+const Note = model("Note", noteSchema)
+
+app.post("/create-note", async (req: Request, res: Response) => {
+    const myNote = new Note({
+        title: "Learning Mongoose",
+        content: "I am Learning Mongoose"
+    })
+
+    await myNote.save()
+    res.status(201).json({
+        success: true,
+        message: "Note created Successfully",
+        note: myNote
+    })
+})
+
 
 app.get("/", (req: Request, res: Response) => {
     res.send('Welcome to Learning Mongoose')
--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,7 @@ const PORT = 5000;
 async function main() {
 
     try {
-        await mongoose.connect('mongodb+srv://mongoose:mongoose@cluster0.uv360.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+        await mongoose.connect('mongodb+srv://mongoose:mongoose@cluster0.uv360.mongodb.net/note-app?retryWrites=true&w=majority&appName=Cluster0');
         console.log("Connected to mongoDB Using Mongoose");
         server = app.listen(PORT, () => {
             console.log(`App is Listening on port ${PORT}`);
