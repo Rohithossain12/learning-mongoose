9c88d20dc3a8037548f30026304fb278c7725193
Rohithossain12 <https://github.com/Rohithossain12>
2025-06-18 13:38:17 +0600

    first commit

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+import express, { Application, Request, Response } from "express";
+
+const app: Application = express();
+
+app.get("/", (req: Request, res: Response) => {
+    res.send('Welcome to Learning Mongoose')
+})
+
+
+
+export default app;
---
+++ src/server.ts
@@ -0,0 +1,24 @@
+import { Server } from "http";
+import app from "./app";
+import mongoose from "mongoose";
+
+let server: Server;
+const PORT = 5000;
+
+
+async function main() {
+
+    try {
+        await mongoose.connect('mongodb+srv://mongoose:mongoose@cluster0.uv360.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+        console.log("Connected to mongoDB Using Mongoose");
+        server = app.listen(PORT, () => {
+            console.log(`App is Listening on port ${PORT}`);
+        })
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+main()
